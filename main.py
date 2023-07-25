21964ced0db9edda9a26315156a5aedb11ea1f2c
Eugenio Petullà <eugenio.petulla@codemotion.it>
2023-07-26 01:30:15 +0200

    Fix the expected plugins.json structure, add a memory cache

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import httpx
 from fastapi import FastAPI, HTTPException, Query
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
 GITHUB_PLUGINS_JSON_URL = "https://raw.githubusercontent.com/cheshire-cat-ai/awesome-plugins/main/plugins.json"
 DEFAULT_PAGE_SIZE = 10
+CACHE_DURATION_MINUTES = 1440  # Set cache duration (1 day in minutes)
 
 
 async def fetch_plugin_json(url):
@@ -13,40 +15,65 @@ async def fetch_plugin_json(url):
         return response.json()
 
 
+# Dictionary to store cached plugin.json data
+cache = {}
+cache_timestamp = {}
+
+
+def is_cache_valid():
+    """
+    Check if the cache is still valid based on the cache duration.
+    """
+    if "plugins" not in cache_timestamp:
+        return False
+    cache_time = cache_timestamp["plugins"]
+    return datetime.utcnow() < cache_time + timedelta(minutes=CACHE_DURATION_MINUTES)
+
+
 @app.get("/plugins")
 async def read_remote_json(page: int = 1, page_size: int = DEFAULT_PAGE_SIZE):
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(GITHUB_PLUGINS_JSON_URL)
-            data = response.json()
+    global cache, cache_timestamp
 
-            total_plugins = len(data)
-            start_index = (page - 1) * page_size
-            end_index = start_index + page_size
+    # Check if cache is still valid
+    if is_cache_valid():
+        cached_plugins = cache["plugins"]
+    else:
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(GITHUB_PLUGINS_JSON_URL)
+                data = response.json()
 
-            if start_index >= total_plugins:
-                return []
+                total_plugins = len(data)
+                start_index = (page - 1) * page_size
+                end_index = start_index + page_size
 
-            plugin_json_data = []
-            for entry in list(data.values())[start_index:end_index]:
-                url = entry["url"]
-                plugin_json_url = url.replace("github.com", "raw.githubusercontent.com") + "/main/plugin.json"
-                try:
-                    plugin_data = await fetch_plugin_json(plugin_json_url)
-                    plugin_json_data.append(plugin_data)
-                except httpx.RequestError as e:
-                    error_msg = f"Error fetching plugin.json for URL: {plugin_json_url}, Error: {str(e)}"
-                    plugin_json_data.append({"error": error_msg})
+                if start_index >= total_plugins:
+                    return []
 
-            return {
-                "total_plugins": total_plugins,
-                "page": page,
-                "page_size": page_size,
-                "plugins": plugin_json_data,
-            }
+                cached_plugins = []
+                for entry in data[start_index:end_index]:
+                    url = entry["url"]
+                    plugin_json_url = url.replace("github.com", "raw.githubusercontent.com") + "/main/plugin.json"
+                    try:
+                        plugin_data = await fetch_plugin_json(plugin_json_url)
+                        cached_plugins.append(plugin_data)
+                    except httpx.RequestError as e:
+                        error_msg = f"Error fetching plugin.json for URL: {plugin_json_url}, Error: {str(e)}"
+                        cached_plugins.append({"error": error_msg})
 
-    except httpx.RequestError as e:
-        raise HTTPException(status_code=500, detail=f"Error fetching GitHub data: {str(e)}")
+                # Update the cache with the new data and timestamp
+                cache["plugins"] = cached_plugins
+                cache_timestamp["plugins"] = datetime.utcnow()
+
+        except httpx.RequestError as e:
+            raise HTTPException(status_code=500, detail=f"Error fetching GitHub data: {str(e)}")
+
+    return {
+        "total_plugins": len(cached_plugins),
+        "page": page,
+        "page_size": page_size,
+        "plugins": cached_plugins,
+    }
 
 
 if __name__ == "__main__":
