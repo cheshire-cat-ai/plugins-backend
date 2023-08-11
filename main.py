64401d6128a25260eedc99e52f43f15d28782f0b
valentimarco <gamespy67@gmail.com>
2023-08-11 17:23:14 +0200

    gived a default list when docker is not used

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI
 import uvicorn
 from endpoints import Endpoints
 
-GITHUB_PLUGINS_JSON_URL = os.environ["GITHUB_PLUGINS_JSON_URL"]
-DEFAULT_PAGE_SIZE = int(os.environ["DEFAULT_PAGE_SIZE"])
-CACHE_DURATION_MINUTES = int(os.environ["CACHE_DURATION_MINUTES"])
-RELOAD = bool(os.environ["RELOAD"])
+GITHUB_PLUGINS_JSON_URL = os.getenv("GITHUB_PLUGINS_JSON_URL", "https://raw.githubusercontent.com/cheshire-cat-ai/awesome-plugins/main/plugins.json")
+DEFAULT_PAGE_SIZE = int(os.getenv("DEFAULT_PAGE_SIZE",10))
+CACHE_DURATION_MINUTES = int(os.getenv("CACHE_DURATION_MINUTES",1440))
+RELOAD = bool(os.getenv("RELOAD", True))
 
 app = FastAPI()
 
